8bb2113d5118f6cba6eba685b26322e983707667
Mikhail _Gotsko <Shturmfcng@gmail.com>
2021-05-01 18:38:10 +0300

    feat: Made the layout of the main page.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,1 @@
 import '../scss/app.scss';
-
-/* Your JS Code goes here */
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@
  * Webpack main configuration file
  */
 
-const path = require("path");
-const fs = require("fs");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { extendDefaultPlugins } = require("svgo");
+const path = require('path');
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { extendDefaultPlugins } = require('svgo');
 
-const environment = require("./configuration/environment");
+const environment = require('./configuration/environment');
 
 const templateFiles = fs
   .readdirSync(environment.paths.source)
-  .filter((file) => path.extname(file).toLowerCase() === ".html");
+  .filter((file) => path.extname(file).toLowerCase() === '.html');
 
 const htmlPluginEntries = templateFiles.map(
   (template) =>
@@ -24,42 +24,37 @@ const htmlPluginEntries = templateFiles.map(
       hash: false,
       filename: template,
       template: path.resolve(environment.paths.source, template),
-      favicon: path.resolve(environment.paths.source, "images", "favicon.ico"),
-    })
+      favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
+    }),
 );
 
 module.exports = {
   entry: {
-    app: path.resolve(environment.paths.source, "js", "app.js"),
+    app: path.resolve(environment.paths.source, 'js', 'app.js'),
   },
   output: {
-    filename: "js/[name].js",
+    filename: 'js/[name].js',
     path: environment.paths.output,
   },
   module: {
     rules: [
       {
         test: /\.((c|sa|sc)ss)$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.(png|gif|jpe?g|svg)$/i,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              name: "images/design/[name].[hash:6].[ext]",
-              publicPath: "../",
+              name: 'images/design/[name].[hash:6].[ext]',
+              publicPath: '../',
               limit: environment.limits.images,
             },
           },
@@ -69,10 +64,10 @@ module.exports = {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              name: "fonts/[name].[hash:6].[ext]",
-              publicPath: "../",
+              name: 'fonts/[name].[hash:6].[ext]',
+              publicPath: '../',
               limit: environment.limits.fonts,
             },
           },
@@ -82,7 +77,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "css/[name].css",
+      filename: 'css/[name].css',
     }),
     new ImageMinimizerPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
@@ -90,15 +85,15 @@ module.exports = {
         // Lossless optimization with custom option
         // Feel free to experiment with options for better result for you
         plugins: [
-          ["gifsicle", { interlaced: true }],
-          ["jpegtran", { progressive: true }],
-          ["optipng", { optimizationLevel: 5 }],
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
           [
-            "svgo",
+            'svgo',
             {
               plugins: extendDefaultPlugins([
                 {
-                  name: "removeViewBox",
+                  name: 'removeViewBox',
                   active: false,
                 },
               ]),
@@ -109,20 +104,20 @@ module.exports = {
     }),
     new CleanWebpackPlugin({
       verbose: true,
-      cleanOnceBeforeBuildPatterns: ["**/*", "!stats.json"],
+      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(environment.paths.source, "images", "content"),
-          to: path.resolve(environment.paths.output, "images", "content"),
-          toType: "dir",
+          from: path.resolve(environment.paths.source, 'images', 'content'),
+          to: path.resolve(environment.paths.output, 'images', 'content'),
+          toType: 'dir',
           globOptions: {
-            ignore: ["*.DS_Store", "Thumbs.db"],
+            ignore: ['*.DS_Store', 'Thumbs.db'],
           },
         },
       ],
     }),
   ].concat(htmlPluginEntries),
-  target: "web",
+  target: 'web',
 };
